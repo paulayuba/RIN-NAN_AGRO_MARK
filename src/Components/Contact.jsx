6a1e60710c575eb9c712ff246366bf90d37dddcf
paulayuba <paulayuba788@gmail.com>
2024-11-26 13:05:57 +0100

    update

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
             </div>
             <div className="mb-4">
               <FaMapMarkedAlt className="inline-block text-green-400 mr-2" />
-              <span className="text-black text-2xl">Bokkos, City, Nigeria</span>
+              <span className="text-black text-2xl">international, City, Nigeria</span>
             </div>
           </div>
 
